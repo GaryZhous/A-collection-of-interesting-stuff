f4d1c855646a4ff06799b83cfa9bd80dad39e3ca
Gary Zhou <117497221+GaryZhous@users.noreply.github.com>
2023-01-05 04:08:32 -0500

    Update 2021EOS_Q7.c

--- 2021EOS_Q7.c
+++ 2021EOS_Q7.c
@@ -12,11 +12,20 @@ return 0;
 }
 
 char* removeStringFromString(char* str, char* substr) {
+  //see if substr is longer than str
+  switch (int a = strcmp(str, substr)){
+    case (-1):{
+      char* err = "substr is longer than str, failed!";
+      return err;}
+    default:{
+      continue;
+    }
+  }
   // base case: if the substring is not present in the string, return the string as is
   if (!strstr(str, substr)) {
     return str;
   }
-
+  
   // recursive case: create a new string with the first instance of the substring removed
   int index = strstr(str, substr) - str;
   int len = strlen(str);
