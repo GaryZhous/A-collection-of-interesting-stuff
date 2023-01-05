30fecbff156ffbc313c5d72c722385309bbab84f
Gary Zhou <117497221+GaryZhous@users.noreply.github.com>
2023-01-05 00:57:53 -0500

    Create 2021EOS_Q7.c
    Complete the definition of a C function called removeStringFromString(), whose prototype is
    shown below, that takes two parameters, a string str and another string substr, and returns a
    new string that contains a copy of str, but with all instances of substr removed. For example, the
    following code in the main function:
    char *s = "Hello World!";
    const char *p = removeStringFromString(s, "l");
    const char *q = removeStringFromString(s, "el");
    printf("%s\n", p);
    printf("%s\n", q);
    would print:
    Heo Word!
    Hlo World!
    You must use recursion to solve this problem.
    Hint: You may wish to define your own helper function.

---
+++ 2021EOS_Q7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+extern char *removeStringFromString(char *str, char * substr);
+int main(){
+  char *s = "Hello World!";
+const char *p = removeStringFromString(s, "l");
+const char *q = removeStringFromString(s, "el");
+printf("%s\n", p);
+printf("%s\n", q);
+return 0;
+}
+
+char* removeStringFromString(char* str, char* substr) {
+  // base case: if the substring is not present in the string, return the string as is
+  if (!strstr(str, substr)) {
+    return str;
+  }
+
+  // recursive case: create a new string with the first instance of the substring removed
+  int index = strstr(str, substr) - str;
+  int len = strlen(str);
+  int sublen = strlen(substr);
+  char* newstr = malloc(len - sublen + 1);
+  strncpy(newstr, str, index);
+  strncpy(newstr + index, str + index + sublen, len - index - sublen);
+  newstr[len - sublen] = '\0';
+  return removeStringFromString(newstr, substr);
+}
