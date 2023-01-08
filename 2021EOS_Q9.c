07a32c9fbdb37dde11327add4079beda6bd32960
Gary Zhou <117497221+GaryZhous@users.noreply.github.com>
2023-01-08 00:28:42 -0500

    Create 2021EOS_Q9.c
    Imagine we have a sorted array of int-type integers, a, in the following form:
    a = {a1, a2, . . . , aN−1, aN }
    where a1 ≤ a2 ≤ . . . ≤ aN−1 ≤ aN . We now define the following array from the sorted array a:
    ad = {a1, aN , a2, aN−1, . . .}
    In this array ad, the smallest item is followed by the greatest item, and then the second smallest
    item and the second greatest item, and so on. We call this special array a Decussate-sorted array.
    You are asked to write a function int *merge(int *size), which receives a series of Decussatesorted arrays of different lengths from user input, and returns a single-dimensional array that
    merges them all together to one Decussate-sorted array. The merge() function has one parameter
    size, which is used to return the size of the merged array to the calling function. Your merge
    function is responsible for getting the array information from the user in the manner shown below.
    Note: Your implementation should have no memory leaks. In other words, any dynamically allocated memory that you use inside the function should be freed. The returned array from the
    merge() function will be freed by the calling function.
    Hint: You may use a sorting algorithm if that simplifies your solution.
    Here is an example main() function that can be used to test your work:

---
+++ 2021EOS_Q9.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+extern int* merge(int* size);
+
+int main(){
+  int size;
+  int *mergedArray = merge(&size);
+  printf("Result: ");
+  for (int i = 0; i < size; i ++) {
+   printf("%d ", mergedArray[i]);
+  }
+  printf("\n");
+  free(mergedArray);
+  return 0;
+
+}
+
+int* merge(int* size){
+  int count = 0;
+  int list[*size];
+  int* new_list = (int*)malloc(*size*sizeof(int));
+  int init = 0;
+  while(count<*size){
+    int load;
+    printf("enter the size of an array:");
+    scanf("%d", &load);
+    for (int i = 0;i<load;i++){
+      printf("enter the numbers:");
+      scanf("%d", &list[init++]);
+    }
+    count += load;
+  }
+
+  int j =0;
+  int i =0;
+  while (i<*size){
+  int max = 0;
+  int min = 100000;
+  for (;i<*size;i++){
+    if(list[i]>max){
+      max = list[i];
+    }
+  }
+  new_list[j++] = max;
+  i+=1;
+  for (;i<*size;i++){
+    if(list[i]<min){
+      min = list[i];
+    }
+  }
+  new_list[j++] = min;
+  i+=1;
+    }
+  return new_list;
+}
