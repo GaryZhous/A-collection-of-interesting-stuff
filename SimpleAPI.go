4f07fcaf064b3899bb883b44082692d204c3da36
Gary Zhou <garyzhou1201@gmail.com>
2026-01-16 00:01:37 -0500

    Fix JSON encoding error in writeJSON function

--- SimpleAPI.go
+++ SimpleAPI.go
@@ -39,7 +39,7 @@ type APIError struct {
 func writeJSON(w http.ResponseWriter, status int, v any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	_ = json.NewEncoder(w).Encode(v)
+	json.NewEncoder(w).Encode(v)
 }
 
 func validate(req ChargeRequest) error {
