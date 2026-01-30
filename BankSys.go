e156a764b9403a3ad0ff0023735e3f59e357d6ad
Gary Zhou <garyzhou1201@gmail.com>
2026-01-30 10:36:30 -0500

    Update BankSys.go

--- BankSys.go
+++ BankSys.go
@@ -1,9 +1,9 @@
-/*
-  You have been tasked with writing a program for a popular bank that will automate all its incoming transactions (transfer, deposit, and withdraw). The bank has n accounts numbered from 1 to n. The initial balance of each account is stored in a 0-indexed integer array balance, with the (i + 1)th account having an initial balance of balance[i].
-  Execute all the valid transactions. A transaction is valid if:
+/**
+  *You have been tasked with writing a program for a popular bank that will automate all its incoming transactions (transfer, deposit, and withdraw). The bank has n accounts numbered from 1 to n. The initial balance of each account is stored in a 0-indexed integer array balance, with the (i + 1)th account having an initial balance of balance[i].
+  *Execute all the valid transactions. A transaction is valid if:
     1. The given account number(s) are between 1 and n, and
     2. The amount of money withdrawn or transferred from is less than or equal to the balance of the account.
-  Implement the Bank class:
+  *Implement the Bank class:
     1. Bank(long[] balance) Initializes the object with the 0-indexed integer array balance.
     2. boolean transfer(int account1, int account2, long money) Transfers money dollars from the account numbered account1 to the account numbered account2. Return true if the transaction was successful, false otherwise.
     3. boolean deposit(int account, long money) Deposit money dollars into the account numbered account. Return true if the transaction was successful, false otherwise.
