75a05b116c2e930b4ac3a64e801c171241793a66
Gary Zhou <garyzhou1201@gmail.com>
2026-03-07 00:01:35 -0500

    Remove usage instructions from RideSharingSys.rs
    Removed comment block with usage instructions.

--- RideSharingSys.rs
+++ RideSharingSys.rs
@@ -1,11 +1,3 @@
-/**
- * Your RideSharingSystem object will be instantiated and called as such:
- * let obj = RideSharingSystem::new();
- * obj.add_rider(riderId);
- * obj.add_driver(driverId);
- * let ret_3: Vec<i32> = obj.match_driver_with_rider();
- * obj.cancel_rider(riderId);
- */
 use std::cell::RefCell;
 use std::collections::{HashSet, VecDeque};
 
