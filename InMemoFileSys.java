8ce6166bc45a18e3222771aeb85e4f40b72e26d1
Gary Zhou <garyzhou1201@gmail.com>
2026-03-31 00:01:05 -0400

    Remove comments from InMemoFileSys.java
    Removed initial comments describing the FileSystem class implementation.

--- InMemoFileSys.java
+++ InMemoFileSys.java
@@ -1,17 +1,3 @@
-/*Design a data structure that simulates an in-memory file system.
-
-  Implement the FileSystem class:
-  1. FileSystem() Initializes the object of the system.
-  2. List<String> ls(String path)
-    If path is a file path, returns a list that only contains this file's name.
-    If path is a directory path, returns the list of file and directory names in this directory.
-    The answer should in lexicographic order.
-  3. void mkdir(String path) Makes a new directory according to the given path. The given directory path does not exist. If the middle directories in the path do not exist, you should create them as well.
-  4. void addContentToFile(String filePath, String content)
-    If filePath does not exist, creates that file containing given content.
-    If filePath already exists, appends the given content to original content.
-  5. String readContentFromFile(String filePath) Returns the content in the file at filePath.
-*/
 public class FileSystem {
     class File {
         boolean isfile = false;
